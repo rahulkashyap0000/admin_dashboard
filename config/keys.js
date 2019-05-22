b3d0c1a4001552cedce6f139ec7bdbcdff1c618a
Rahul Kashyap <rahul.kashyap.17001@iitgoa.ac.in>
2019-05-22 19:38:36 +0530

    Created User Schema

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://rahul:rahul@cluster0-jubeg.mongodb.net/admin?retryWrites=true"
+    "mongodb+srv://rahul:rahul@cluster0-jubeg.mongodb.net/admin_database?retryWrites=true"
 };
