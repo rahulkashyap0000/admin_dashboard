ba2df7cdb90b09142410eb50bbc54105fd61b319
Rahul Kashyap <rahul.kashyap.17001@iitgoa.ac.in>
2019-05-22 20:29:24 +0530

    Added login case
    route localhost:5000/api/users/login returns message "Success". Now, I have to send back a json web token

--- routes/api/users.js
+++ routes/api/users.js
@@ -38,4 +38,28 @@ router.post("/register", (req, res) => {
   });
 });
 
+// route:       GET api/users/login
+// description: To return json web token
+// access:      Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Compare Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
